9aa045639178e2ff631736a88ff9f0a21254a267
Xasan <xasanasqarov15@gmail.com>
2024-01-08 15:37:17 +0500

    test

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,11 +1,3 @@
-import React, { Component } from 'react'
-
-export class button extends Component {
-  render() {
-    return (
-      <div>button</div>
-    )
-  }
+export function button() {
+  return <div>button</div>;
 }
-
-export default button

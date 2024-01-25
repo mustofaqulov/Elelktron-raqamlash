2ee17b59a5377f722f0b086590937909cbbd21f5
Islombek <mustofaqulov079@gmail.com>
2024-01-25 23:11:40 +0500

    created input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { OptionInput } from "./components/Inputs/Option-input/Option-input";
 
+import "./styles/style.scss";
 function App() {
-
   return (
     <>
-      <h1>Hello</h1>
+      <OptionInput
+        placeholder="tuman..."
+        label="Tuman"
+        classInp="textInput"
+        type="text"
+        id="Қашқадарё_вилояти"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

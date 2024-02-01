9110e67652427ad00030b7469086a434d3fdddf9
Xasan <xasanasqarov15@gmail.com>
2024-02-01 11:20:38 +0500

    fix: HomePage and Modal fix design

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,24 +7,26 @@ export function Modal({ open, onClose }) {
   return (
     <>
       <div onClick={onClose} className="close_modal"></div>
-      <div className="modal">
-        <div className="close-content">
-          <div onClick={onClose} className="close">
-            <span class="material-symbols-outlined">close</span>
+      <div className="bg-modal">
+        <div className="modal">
+          <div className="close-content">
+            <div onClick={onClose} className="close">
+              <span class="material-symbols-outlined">close</span>
+            </div>
           </div>
-        </div>
-        <div className="modal-login">
-          <h2>Login</h2>
-          <div className="content-box">
-            <label htmlFor="email">Email</label>
-            <input autoComplete="off" id="email" type="email" />
-          </div>
-          <div className="content-box">
-            <label htmlFor="email">Password</label>
-            <input autoComplete="off" id="email" type="password" />
-          </div>
-          <div className="login-btn">
-            <Button title="Tizimga kirish" icon={<OpenIcon />} />
+          <div className="modal-login">
+            <h2>Login</h2>
+            <div className="content-box">
+              <label htmlFor="email">Email</label>
+              <input autoComplete="off" id="email" type="email" />
+            </div>
+            <div className="content-box">
+              <label htmlFor="email">Password</label>
+              <input autoComplete="off" id="email" type="password" />
+            </div>
+            <div className="login-btn">
+              <Button title="Tizimga kirish" icon={<OpenIcon />} />
+            </div>
           </div>
         </div>
       </div>

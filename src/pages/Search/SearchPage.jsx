414a598cec871f71e478cbf24d7f9da185abe15f
Islombek <mustofaqulov079@gmail.com>
2024-02-28 14:46:23 +0500

    fixed bugs

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -5,7 +5,7 @@ import { Header } from "../../components/Header/Header";
 import { Button } from "../../components/Button/Button";
 import { Link } from "react-router-dom";
 import { InputOrg } from "../../components/Inputs/InputOrg/InputOrg";
-import { data } from "../../JSON/data";
+import { data } from "../../utils/mocks/data";
 import Plus from "../../assets/icon/add.svg?react";
 import Search from "../../assets/icon/search.svg?react";
 
